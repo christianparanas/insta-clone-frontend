79685f076ab5c73bea6aad3cf050697c55b97f98
Christian Paranas <christiannparanas@gmail.com>
2021-05-27 21:28:21 +0800

    add story btn

--- components/home/Nav.tsx
+++ components/home/Nav.tsx
@@ -11,7 +11,6 @@ export default function Nav() {
         </div> 
         <svg aria-label="Direct" className="_8-yf5 " fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"></path></svg>
       </div>
-
       <div className="bottom_nav_container">
         <div className="nav_bottom_btn">
           <svg aria-label="Home" className="_8-yf5 " fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path d="M45.5 48H30.1c-.8 0-1.5-.7-1.5-1.5V34.2c0-2.6-2.1-4.6-4.6-4.6s-4.6 2.1-4.6 4.6v12.3c0 .8-.7 1.5-1.5 1.5H2.5c-.8 0-1.5-.7-1.5-1.5V23c0-.4.2-.8.4-1.1L22.9.4c.6-.6 1.6-.6 2.1 0l21.5 21.5c.3.3.4.7.4 1.1v23.5c.1.8-.6 1.5-1.4 1.5z"></path></svg>
@@ -32,3 +31,4 @@ export default function Nav() {
     </div>
   )
 }
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import Head from 'next/head'
+import { useState, useRef } from 'react'
 
 
 // components
 import Nav from '../components/home/Nav';
 
 export default function Home() {
+  const fileInput = useRef(null);
+
+
+  // open add story file dialog on click
+  const open_fileInput_dialog = () => {
+    fileInput.current?.click();
+  }
+
   return (
     <div className="container">
       <Head>
@@ -15,7 +24,19 @@ export default function Home() {
       <div className="home_container">
         <Nav />
 
-        dad
+        <div className="home_stories_container">
+          <div onClick={open_fileInput_dialog} className="home_user_add_story">
+            <div className="img_btn">
+              <img src="/Default.jpg" alt="" />
+              <svg aria-label="Plus icon" fill="#0095f6" height="16" viewBox="0 0 48 48" width="16"><path d="M24 0C10.8 0 0 10.7 0 24s10.7 24 24 24 24-10.7 24-24S37.3 0 24 0zm12.3 25.5H25.5v10.7c0 .8-.7 1.5-1.5 1.5s-1.5-.7-1.5-1.5V25.5H11.8c-.8 0-1.5-.7-1.5-1.5s.7-1.5 1.5-1.5h10.7V11.7c0-.8.7-1.5 1.5-1.5s1.5.7 1.5 1.5v10.7h10.7c.8 0 1.5.7 1.5 1.5s-.6 1.6-1.4 1.6z"></path></svg>
+            </div>
+            <p>Your Story</p>
+            <form method="POST">
+              <input ref={fileInput} accept="image/jpeg" type="file" />
+            </form>
+          </div>
+        </div>
+        sd
       </div>
     </div>
   )
