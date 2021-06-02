2c61cdefd096f7430a01b89c7e9d1e9f2d401434
Christian Paranas <christiannparanas@gmail.com>
2021-06-02 21:15:53 +0800

    bsse

--- pages/create.tsx
+++ pages/create.tsx
@@ -3,34 +3,48 @@ import { useForm } from "react-hook-form";
 import FileBase from 'react-file-base64'
 
 
+
 export default function create() {
 
-	const [postData, setPostData] = useState({
-		img: '',
-	})
-
-
+	const [postImgData, setPostImgData] = useState([])
+                                                                                                                             
 
 	const handleSubmit = (event) => {
-		console.log(postData)
+		console.log(postImgData)
 		event.preventDefault();
 
+	}
+
+	const postDataSetter = (base64) => {
+		console.log(base64)
+		setPostImgData(base64)
 	}
 
 	return (
 		<>
 			<form onSubmit={handleSubmit}>
 				<FileBase 
-					type="file" 
-					multiple={false} 
-					onDone={({base64}) => setPostData({ ...postData, img: base64 })}
+					multiple={true}
+					onDone={(base64) => postDataSetter(base64)}
 				/>
 				<input type="submit" name="" id="" />
 			</form>
 
-			{postData && (
-				<img src={`${postData.img}`} alt="" />
-			)}
+				{postImgData.map((val, key) => {
+					return (
+						<div key={key}>
+							<img src={`${val.base64}`} alt="" />
+						</div>
+					)
+				})}
+
 		</>
 	)
 }
+
+
+// {postData && (
+// 	<video src={`${postData.img}`} type="video/mp4" width="320" height="240" controls>
+// 		hello
+// 	</video>
+// )}
