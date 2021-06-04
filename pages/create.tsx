694bfe9c3a58a0f9654448a7b62f01c8f043c4d0
Yuri Orlov <christiannparanas@gmail.com>
2021-06-04 20:25:43 +0800

    Storing img data arr to postData state

--- pages/create.tsx
+++ pages/create.tsx
@@ -9,15 +9,20 @@ export default function create() {
 	const router = useRouter()
 
 	const { images, updateImgState } = postData()
+        const [postData, setPostData] = useState([])
 
   const returnee = () => {
   	router.push('/')
   }
 
+  useEffect(() => {
+    setPostData(images)
+  }, [])
+
 	return (
 		<div className="create_container">
 			<div onClick={returnee} className="back">Return</div>
-			{images.map((val, key) => {
+			{postData.map((val, key) => {
 					return (
 						<div key={key}>
 							<img src={`${val}`} alt="" />
