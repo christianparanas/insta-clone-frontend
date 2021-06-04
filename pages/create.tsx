5c2490ca012282be99044a81d05df0ac3180a079
Yuri Orlov <christiannparanas@gmail.com>
2021-06-04 20:28:53 +0800

    Update create.tsx

--- pages/create.tsx
+++ pages/create.tsx
@@ -9,20 +9,20 @@ export default function create() {
 	const router = useRouter()
 
 	const { images, updateImgState } = postData()
-        const [postData, setPostData] = useState([])
+        const [postDatas, setPostDatas] = useState([])
 
   const returnee = () => {
   	router.push('/')
   }
 
   useEffect(() => {
-    setPostData(images)
+    setPostDatas(images)
   }, [])
 
 	return (
 		<div className="create_container">
 			<div onClick={returnee} className="back">Return</div>
-			{postData.map((val, key) => {
+			{postDatas.map((val, key) => {
 					return (
 						<div key={key}>
 							<img src={`${val}`} alt="" />
